5b86e4debe08e209170d8630be69a7865382a0b1
SaiiintCHain <zok-hor@mail.ru>
2024-11-17 20:39:47 +0300

    Add files via upload

---
+++ mistakes in stack.py
@@ -0,0 +1,33 @@
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+    for i in numbers:
+        try:
+            result += i
+        except TypeError:
+            print(f'Некорректный тип данных для подсчёта суммы - {i}')
+            incorrect_data += 1
+    return result, incorrect_data
+
+def calculate_average(numbers):
+    if not isinstance(numbers, (list, tuple)):
+        print('В numbers записан некорректный тип данных')
+        return None
+
+    sum_result, incorrect_data = personal_sum(numbers)
+    try:
+        count = len(numbers) - incorrect_data
+        if count == 0:
+            raise ZeroDivisionError("Делить на ноль нельзя")
+        a = (sum_result / count)
+        return a
+    except ZeroDivisionError as exc:
+        print(f'{exc} хотела нас сломать, но мы устояли')
+        return 0
+
+
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
